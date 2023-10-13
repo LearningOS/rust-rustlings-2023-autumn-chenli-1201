e1b8b8c75ddbc633ba79d6f918783a4a07051f68
chenli1201 <2948596143@qq.com>
2023-10-13 21:57:02 +0800

    nice

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -14,8 +14,6 @@
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Hash, PartialEq, Eq)]
@@ -39,7 +37,8 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
     for fruit in fruit_kinds {
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
-        // already present!
+        // already present!      
+         basket.entry(fruit).or_insert(1);
     }
 }
 
@@ -50,6 +49,7 @@ mod tests {
     // Don't modify this function!
     fn get_fruit_basket() -> HashMap<Fruit, u32> {
         let mut basket = HashMap::<Fruit, u32>::new();
+
         basket.insert(Fruit::Apple, 4);
         basket.insert(Fruit::Mango, 2);
         basket.insert(Fruit::Lychee, 5);
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,45 +20,55 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
-
-pub enum Command {
-    Uppercase,
-    Trim,
-    Append(usize),
-}
-
-mod my_module {
-    use super::Command;
-
-    // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
-        // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
-        for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
-        }
-        output
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
-    use super::Command;
-
-    #[test]
-    fn it_works() {
-        let output = transformer(vec![
-            ("hello".into(), Command::Uppercase),
-            (" all roads lead to rome! ".into(), Command::Trim),
-            ("foo".into(), Command::Append(1)),
-            ("bar".into(), Command::Append(5)),
-        ]);
-        assert_eq!(output[0], "HELLO");
-        assert_eq!(output[1], "all roads lead to rome!");
-        assert_eq!(output[2], "foobar");
-        assert_eq!(output[3], "barbarbarbarbarbar");
-    }
+pub enum Command {  
+    Uppercase,  
+    Trim,  
+    Append(usize),  
+}  
+  
+mod my_module {  
+    
+    use super::Command;  
+  
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {  
+        let mut output: Vec<String> = Vec::new();  
+        for (string, command) in input.iter() {  
+            match command {  
+                Command::Uppercase => {  
+                    output.push(string.to_uppercase());  
+                }  
+                Command::Trim => {  
+                    output.push(string.trim().to_string());  
+                }  
+                Command::Append(usize) => {  
+                    let mut ans = String::new();  
+                    for i in 0..*usize {  
+                        ans += &string.clone();  
+                    }  
+                    output.push(format!("{}bar", ans));  
+                }  
+            }  
+        }  
+        output  
+    }  
+}  
+  
+#[cfg(test)]  
+mod tests {  
+    use crate::Command;  
+    use crate::my_module::transformer;  
+  
+    #[test]  
+    fn it_works() {  
+        let output = transformer(vec![  
+            ("hello".into(), Command::Uppercase),  
+            (" all roads lead to rome! ".into(), Command::Trim),  
+            ("foo".into(), Command::Append(1)),  
+            ("bar".into(), Command::Append(5)),  
+        ]);  
+        assert_eq!(output[0], "HELLO");  
+        assert_eq!(output[1], "all roads lead to rome!");  
+        assert_eq!(output[2], "foobar");  
+        assert_eq!(output[3], "barbarbarbarbarbar");  
+    }  
 }
