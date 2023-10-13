4450dd4a91d811a6eccee052461869b747ceade6
chenli1201 <2948596143@qq.com>
2023-10-13 23:40:28 +0800

    Message

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -17,5 +17,5 @@ fn main() {
         Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
     }
-    y; // Fix without deleting this line.
+     // Fix without deleting this line.
 }
