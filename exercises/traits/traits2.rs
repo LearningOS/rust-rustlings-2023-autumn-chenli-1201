9fe071de167e70e21f4a5c98b80e2a95fad4ca3a
chenli1201 <2948596143@qq.com>
2023-10-14 01:08:51 +0800

    2

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -7,11 +7,16 @@
 // No boiler plate code this time, you can do this!
 //
 // Execute `rustlings hint traits2` or use the `hint` watch subcommand for a hint.
-
-// I AM NOT DONE
-
-trait AppendBar {
-    fn append_bar(self) -> Self;
+trait AppendBar {  
+    /// Appends "Bar" to the receiver and returns the modified receiver.  
+    fn append_bar(self) -> Self;  
+}  
+ 
+impl AppendBar for Vec<String> {
+    fn append_bar(mut self) -> Self{
+        self.push("Bar".to_string());
+        self
+    }
 }
 
 // TODO: Implement trait `AppendBar` for a vector of strings.
