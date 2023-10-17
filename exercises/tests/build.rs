aeaca2ccaaee34ed74c253aaede8685ecf7adadb
chenli1201 <2948596143@qq.com>
2023-10-17 19:41:30 +0800

    Message

--- exercises/tests/build.rs
+++ exercises/tests/build.rs
@@ -9,12 +9,11 @@ fn main() {
         .duration_since(std::time::UNIX_EPOCH)  
         .unwrap()  
         .as_secs();  
-    let your_command = format!("TEST_FOO={}", timestamp);  
-    println!("cargo:rustc-env={}", your_command);  
+    let your_command1 = format!("TEST_FOO={}", timestamp);  
+    println!("cargo:rustc-env={}", your_command1);  
   
-    // In tests8, we should enable "pass" feature to make the  
-    // testcase return early. Fill in the command to tell  
-    // Cargo about that.  
-    let your_command = pass;
-    println!("cargo:rustc-cfg={}",your_command);  
-}
+        // In tests8, we should enable "pass" feature to make the    
+        // testcase return early. Fill in the command to tell    
+        // Cargo about that.    
+       
+        println!("cargo:rustc-cfg'feature=pass'");   }
